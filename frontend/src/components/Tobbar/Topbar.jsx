573510ee9a4dd4dbb4a199ddb8cef6209950ae77
kibria <shan2neo@gmail.com>
2021-08-26 20:07:05 +0600

    setting up react side

---
+++ frontend/src/components/Tobbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import './topbar.css';
+
+function Topbar() {
+    return (
+      <>
+      <div className="topbar">
+
+      <div className="topbarLeft">
+        <div className="logo">
+            DemoSocial
+        </div>
+      </div>
+      <div className="topbarCenter">
+           
+      </div>
+      <div className="topbarRight"></div>
+     
+
+
+      </div>
+      </>
+    )
+}
+
+export default Topbar
